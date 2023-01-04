a346f4700afee06bdcd0a558fa913eddb0e3a1dd
wjlee611 <wjlee611@gmail.com>
2023-01-04 22:47:16 +0900

    add class image

--- lib/screens/operator_screen.dart
+++ lib/screens/operator_screen.dart
@@ -41,30 +41,57 @@ class _OperatorScreenState extends State<OperatorScreen> {
             header: Container(
               height: 50.0,
               color: Colors.blueGrey.shade700,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
               alignment: Alignment.centerLeft,
-              child: Text(
-                index == 0
-                    ? OperatorModel.vanguard
-                    : index == 1
-                        ? OperatorModel.sniper
-                        : index == 2
-                            ? OperatorModel.guard
-                            : index == 3
-                                ? OperatorModel.caster
-                                : index == 4
-                                    ? OperatorModel.defender
-                                    : index == 5
-                                        ? OperatorModel.medic
-                                        : index == 6
-                                            ? OperatorModel.specialist
-                                            : OperatorModel.supporter,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontFamily: FontFamily.nanumGothic,
-                  fontWeight: FontWeight.w700,
-                ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    index == 0
+                        ? "assets/images/class_vanguard.png"
+                        : index == 1
+                            ? "assets/images/class_sniper.png"
+                            : index == 2
+                                ? "assets/images/class_guard.png"
+                                : index == 3
+                                    ? "assets/images/class_caster.png"
+                                    : index == 4
+                                        ? "assets/images/class_defender.png"
+                                        : index == 5
+                                            ? "assets/images/class_medic.png"
+                                            : index == 6
+                                                ? "assets/images/class_specialist.png"
+                                                : "assets/images/class_supporter.png",
+                    width: 30,
+                    height: 30,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    index == 0
+                        ? OperatorModel.vanguard
+                        : index == 1
+                            ? OperatorModel.sniper
+                            : index == 2
+                                ? OperatorModel.guard
+                                : index == 3
+                                    ? OperatorModel.caster
+                                    : index == 4
+                                        ? OperatorModel.defender
+                                        : index == 5
+                                            ? OperatorModel.medic
+                                            : index == 6
+                                                ? OperatorModel.specialist
+                                                : OperatorModel.supporter,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: FontFamily.nanumGothic,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
               ),
             ),
             content: ClassListView(classIdx: index),
