7bd94428a895a7411305750bcbc0cfb6beae5b98
wjlee611 <wjlee611@gmail.com>
2023-01-04 19:16:02 +0900

    operator data fetch

---
+++ lib/models/operator_model.dart
@@ -0,0 +1,64 @@
+class OperatorModel {
+  final String name, rare, position, detailPos, trait;
+  final List<_TalentModel> talent;
+  final List<_SkillModel> skill;
+  final List<_ModuleModel> module;
+
+  OperatorModel.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        rare = json['rare'],
+        position = json['position'],
+        detailPos = json['detail_pos'],
+        trait = json['trait'],
+        talent = [
+          for (var talJson in json['talent']) _TalentModel.fromJson(talJson)
+        ],
+        skill = [
+          for (var skillJson in json['skill']) _SkillModel.fronJson(skillJson)
+        ],
+        module = [
+          for (var moduleJson in json['module'])
+            _ModuleModel.fromJson(moduleJson)
+        ];
+}
+
+class _TalentModel {
+  final String name, info;
+
+  _TalentModel.fromJson(Map<String, dynamic> json)
+      : name = json['talent_name'],
+        info = json['talent_info'];
+}
+
+class _SkillModel {
+  final String name, sp, duration, type, info;
+
+  _SkillModel.fronJson(Map<String, dynamic> json)
+      : name = json['skill_name'],
+        sp = json['sp'],
+        duration = json['duration'],
+        type = json['type'],
+        info = json['info'];
+}
+
+class _ModuleModel {
+  final String name, code;
+  final List<_ModuleEffectModel> effect;
+
+  _ModuleModel.fromJson(Map<String, dynamic> json)
+      : name = json['module_name'],
+        code = json['module_code'],
+        effect = [
+          for (var effJson in json['effect'])
+            _ModuleEffectModel.fromJson(effJson)
+        ];
+}
+
+class _ModuleEffectModel {
+  final String stat;
+  final _TalentModel talent;
+
+  _ModuleEffectModel.fromJson(Map<String, dynamic> json)
+      : stat = json['stat'],
+        talent = _TalentModel.fromJson(json['talent']);
+}
--- lib/screens/operator_screen.dart
+++ lib/screens/operator_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:Arkhive/models/font_family.dart';
+import 'package:Arkhive/models/operator_model.dart';
 import 'package:Arkhive/widgets/nav_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OperatorScreen extends StatefulWidget {
   const OperatorScreen({super.key});
@@ -11,6 +14,22 @@ class OperatorScreen extends StatefulWidget {
 
 class _OperatorScreenState extends State<OperatorScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<List<OperatorModel>> readOperatorJson() async {
+    List<OperatorModel> operators = [];
+    final String res =
+        await rootBundle.loadString('assets/json/data_operator.json');
+    final data = await json.decode(res)['data'];
+    for (var jsonData in data) {
+      operators.add(OperatorModel.fromJson(jsonData));
+    }
+    return operators;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +50,24 @@ class _OperatorScreenState extends State<OperatorScreen> {
           onPressed: () => scaffoldKey.currentState!.openDrawer(),
         ),
       ),
-      body: const Text('오퍼레이터'),
+      body: FutureBuilder(
+        future: readOperatorJson(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                final operator_ = snapshot.data![index];
+                return Text(operator_.name);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 10,
+              ),
+              itemCount: snapshot.data!.length,
+            );
+          }
+          return const Text('...');
+        },
+      ),
       drawer: const NavDrawer(),
     );
   }
