3307fc6b78e168e27118f62c4b4f6163266d669b
wjlee611 <wjlee611@gmail.com>
2023-01-07 00:41:09 +0900

    apply sliver sticky header

--- lib/screens/operator_screen.dart
+++ lib/screens/operator_screen.dart
@@ -3,7 +3,7 @@ import 'package:arkhive/models/operator_model.dart';
 import 'package:arkhive/tools/open_detail_screen.dart';
 import 'package:arkhive/widgets/nav_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:sticky_headers/sticky_headers.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import '../global_vars.dart' as globals;
 
 class OperatorScreen extends StatefulWidget {
@@ -35,108 +35,106 @@ class _OperatorScreenState extends State<OperatorScreen> {
           onPressed: () => scaffoldKey.currentState!.openDrawer(),
         ),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.zero,
-        physics: const AlwaysScrollableScrollPhysics(),
-        itemBuilder: (context, index) {
-          return StickyHeader(
-            header: Container(
-              height: 50.0,
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Colors.blueGrey.shade700,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    blurRadius: 2,
-                    spreadRadius: 2,
-                    offset: const Offset(
-                      0,
-                      1,
-                    ),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      index == 0
-                          ? "assets/images/class_vanguard.png"
-                          : index == 1
-                              ? "assets/images/class_sniper.png"
-                              : index == 2
-                                  ? "assets/images/class_guard.png"
-                                  : index == 3
-                                      ? "assets/images/class_caster.png"
-                                      : index == 4
-                                          ? "assets/images/class_defender.png"
-                                          : index == 5
-                                              ? "assets/images/class_medic.png"
-                                              : index == 6
-                                                  ? "assets/images/class_specialist.png"
-                                                  : "assets/images/class_supporter.png",
-                      width: 30,
-                      height: 30,
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            index == 0
-                                ? OperatorModel.vanguard
-                                : index == 1
-                                    ? OperatorModel.sniper
-                                    : index == 2
-                                        ? OperatorModel.guard
-                                        : index == 3
-                                            ? OperatorModel.caster
-                                            : index == 4
-                                                ? OperatorModel.defender
-                                                : index == 5
-                                                    ? OperatorModel.medic
-                                                    : index == 6
-                                                        ? OperatorModel
-                                                            .specialist
-                                                        : OperatorModel
-                                                            .supporter,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontFamily: FontFamily.nanumGothic,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Text(
-                            "${globals.classedOperators[index].length}",
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontFamily: FontFamily.nanumGothic,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
+      body: CustomScrollView(
+        slivers: [
+          for (int index = 0; index < globals.classedOperators.length; index++)
+            SliverStickyHeader(
+              header: Container(
+                height: 50.0,
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey.shade700,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                      offset: const Offset(
+                        0,
+                        1,
                       ),
                     ),
                   ],
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        index == 0
+                            ? "assets/images/class_vanguard.png"
+                            : index == 1
+                                ? "assets/images/class_sniper.png"
+                                : index == 2
+                                    ? "assets/images/class_guard.png"
+                                    : index == 3
+                                        ? "assets/images/class_caster.png"
+                                        : index == 4
+                                            ? "assets/images/class_defender.png"
+                                            : index == 5
+                                                ? "assets/images/class_medic.png"
+                                                : index == 6
+                                                    ? "assets/images/class_specialist.png"
+                                                    : "assets/images/class_supporter.png",
+                        width: 30,
+                        height: 30,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              index == 0
+                                  ? OperatorModel.vanguard
+                                  : index == 1
+                                      ? OperatorModel.sniper
+                                      : index == 2
+                                          ? OperatorModel.guard
+                                          : index == 3
+                                              ? OperatorModel.caster
+                                              : index == 4
+                                                  ? OperatorModel.defender
+                                                  : index == 5
+                                                      ? OperatorModel.medic
+                                                      : index == 6
+                                                          ? OperatorModel
+                                                              .specialist
+                                                          : OperatorModel
+                                                              .supporter,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: FontFamily.nanumGothic,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Text(
+                              "${globals.classedOperators[index].length}",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: FontFamily.nanumGothic,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
+              sliver: ClassListView(classIdx: index),
             ),
-            content: ClassListView(classIdx: index),
-          );
-        },
-        itemCount: 8,
+        ],
       ),
       drawer: const NavDrawer(),
     );
@@ -153,12 +151,9 @@ class ClassListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: globals.classedOperators[classIdx].length * 50,
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        physics: const ClampingScrollPhysics(),
-        itemBuilder: (context, index) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
           return GestureDetector(
             onTap: () => OpenDetailScreen.onOperatorTab(
               list: globals.classedOperators[classIdx],
@@ -215,7 +210,7 @@ class ClassListView extends StatelessWidget {
             ),
           );
         },
-        itemCount: globals.classedOperators[classIdx].length,
+        childCount: globals.classedOperators[classIdx].length,
       ),
     );
   }
