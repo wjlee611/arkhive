e9307feddf5274d8d648820df3da13385cedc471
wjlee611 <wjlee611@gmail.com>
2023-01-09 21:49:40 +0900

    change global variable initialize point

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,12 +1,6 @@
-import 'dart:convert';
-
-import 'package:arkhive/models/enemy_model.dart';
 import 'package:arkhive/models/font_family.dart';
-import 'package:arkhive/models/operator_model.dart';
 import 'package:arkhive/widgets/nav_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import '../global_vars.dart' as globals;
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -17,62 +11,6 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  // Loading data
-  Future<void> readOperatorJson() async {
-    final String res =
-        await rootBundle.loadString('assets/json/data_operator.json');
-    final data = await json.decode(res)['data'];
-
-    for (var jsonData in data) {
-      OperatorModel operator_ = OperatorModel.fromJson(jsonData);
-      if (operator_.class_ == OperatorPositions.vanguard) {
-        globals.classedOperators[0].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.guard) {
-        globals.classedOperators[1].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.defender) {
-        globals.classedOperators[2].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.sniper) {
-        globals.classedOperators[3].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.caster) {
-        globals.classedOperators[4].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.medic) {
-        globals.classedOperators[5].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.supporter) {
-        globals.classedOperators[6].add(operator_);
-      }
-      if (operator_.class_ == OperatorPositions.specialist) {
-        globals.classedOperators[7].add(operator_);
-      }
-    }
-  }
-
-  Future<void> readEnemyJson() async {
-    final String res =
-        await rootBundle.loadString('assets/json/data_enemy.json');
-    final data = await json.decode(res)['data'];
-
-    for (var jsonData in data) {
-      globals.enemies.add(EnemyModel.fromJson(jsonData));
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    if (globals.classedOperators[0].isEmpty) {
-      readOperatorJson();
-    }
-    if (globals.enemies.isEmpty) {
-      readEnemyJson();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
