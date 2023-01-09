5fb0ac7fa3790b4014e93d9139b7eba9ab49f53a
wjlee611 <wjlee611@gmail.com>
2023-01-09 17:53:18 +0900

    enemy screen designed

--- lib/screens/enemy_screen.dart
+++ lib/screens/enemy_screen.dart
@@ -1,3 +1,4 @@
+import 'package:arkhive/models/enemy_model.dart';
 import 'package:arkhive/models/font_family.dart';
 import 'package:arkhive/widgets/nav_widget.dart';
 import 'package:flutter/material.dart';
@@ -42,10 +43,44 @@ class _EnemyScreenState extends State<EnemyScreen> {
         ),
         itemBuilder: (context, index) {
           return Card(
+            color: globals.enemies[index].enemyType == EnemyType.elite
+                ? Colors.deepOrange
+                : globals.enemies[index].enemyType == EnemyType.boss
+                    ? Colors.purple
+                    : Colors.white,
             margin: const EdgeInsets.all(5),
-            elevation: 1,
-            child: Image.asset(
-                'assets/images/enemies/${globals.enemies[index].code}.png'),
+            elevation: 5,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              children: [
+                Image.asset(
+                    'assets/images/enemies/${globals.enemies[index].code}.png'),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 4, 4),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 1,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: globals.enemies[index].enemyType == EnemyType.elite
+                        ? Colors.deepOrange.withOpacity(0.8)
+                        : globals.enemies[index].enemyType == EnemyType.boss
+                            ? Colors.purple.withOpacity(0.8)
+                            : Colors.black.withOpacity(0.5),
+                  ),
+                  child: Text(
+                    globals.enemies[index].code.replaceAll('_', '*'),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: FontFamily.nanumGothic,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         },
         itemCount: globals.enemies.length,
