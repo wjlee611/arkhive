6dfb056d0d5835e6657db9052321dab27b8e9a2f
wjlee611 <wjlee611@gmail.com>
2023-01-11 23:38:00 +0900

    fix: 정예 -> 엘리트

--- lib/tools/willpop_function.dart
+++ lib/tools/willpop_function.dart
@@ -1,18 +1,27 @@
+import 'package:arkhive/models/font_family.dart';
 import 'package:flutter/material.dart';
 
 DateTime? _currentBackPressTime;
 
 class WillPopFunction {
-  static Future<bool> onWillPop({required BuildContext context}) {
+  static Future<bool> onWillPop({required BuildContext context}) async {
     DateTime now = DateTime.now();
     if (_currentBackPressTime == null ||
         now.difference(_currentBackPressTime!) > const Duration(seconds: 2)) {
       _currentBackPressTime = now;
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: const Text('종료할까요?'),
+          content: const Text(
+            '종료할까요?',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontFamily: FontFamily.nanumGothic,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
           duration: const Duration(seconds: 2),
-          backgroundColor: Colors.blueGrey.shade700,
+          backgroundColor: Colors.black.withOpacity(0.5),
         ),
       );
       return Future.value(false);
