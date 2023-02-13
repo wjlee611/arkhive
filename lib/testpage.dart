a22b53e3e0a2e5d9824620d96b37073cea299d1f
wjlee611 <wjlee611@gmail.com>
2023-02-13 19:26:25 +0900

    description text formatting

--- lib/testpage.dart
+++ lib/testpage.dart
@@ -110,6 +110,15 @@ class _TestPageScreenState extends State<TestPageScreen> {
     return result;
   }
 
+  Map<String, double> _blackboardListToMap(
+      {required List<BlackboardModel> blackboards}) {
+    Map<String, double> result = {
+      for (var data in blackboards) data.key: data.value
+    };
+
+    return result;
+  }
+
   String _skillLevelSelector(int level) {
     if (level > 7) {
       return "Level 7 Mastery ${level - 7}";
@@ -216,14 +225,25 @@ class _TestPageScreenState extends State<TestPageScreen> {
                                 currLevel: _level.toInt(),
                               )!
                                   .name),
-                              Text(_reqPotentalRankSelector<
-                                      OperatorTalentsCandidatesModel>(
-                                candidates: talent.candidates,
-                                currPot: _potential,
-                                currElite: _elite,
-                                currLevel: _level.toInt(),
-                              )!
-                                  .description),
+                              FormattedTextWidget(
+                                text: _reqPotentalRankSelector<
+                                        OperatorTalentsCandidatesModel>(
+                                  candidates: talent.candidates,
+                                  currPot: _potential,
+                                  currElite: _elite,
+                                  currLevel: _level.toInt(),
+                                )!
+                                    .description,
+                                variables: _blackboardListToMap(
+                                    blackboards: _reqPotentalRankSelector<
+                                            OperatorTalentsCandidatesModel>(
+                                  candidates: talent.candidates,
+                                  currPot: _potential,
+                                  currElite: _elite,
+                                  currLevel: _level.toInt(),
+                                )!
+                                        .blackboard),
+                              ),
                               const SizedBox(height: 10),
                             ],
                           ),
@@ -239,15 +259,17 @@ class _TestPageScreenState extends State<TestPageScreen> {
                           });
                         },
                       ),
-                      Text(skadiSkills
-                          .last.levels[_skillLevel.floor() - 1].description),
+                      FormattedTextWidget(
+                        text: skadiSkills
+                            .last.levels[_skillLevel.floor() - 1].description,
+                        variables: _blackboardListToMap(
+                            blackboards: skadiSkills.last
+                                .levels[_skillLevel.toInt() - 1].blackboard),
+                      ),
                       Text(
                           "SP ${skadiSkills.last.levels[_skillLevel.toInt() - 1].spData.initSp} > ${skadiSkills.last.levels[_skillLevel.toInt() - 1].spData.spCost}"),
                       Text(
                           'Duration ${skadiSkills.last.levels[_skillLevel.toInt() - 1].duration.toStringAsFixed(0)}s'),
-                      for (var board in skadiSkills
-                          .last.levels[_skillLevel.toInt() - 1].blackboard)
-                        Text("${board.key} val: ${board.value}"),
                       const SizedBox(height: 10),
                       const Text('module 2'),
                       Text(skadiModule2!.uniEquipName),
@@ -270,6 +292,36 @@ class _TestPageScreenState extends State<TestPageScreen> {
                       ),
                       for (var part
                           in skadiModule2Data!.phases[_modulePhase].parts)
+                        if (part.target == "TRAIT")
+                          if (_reqPotentalRankSelector<
+                                  ModuleTraitDataBundleModel>(
+                                candidates: part.overrideTraitDataBundle,
+                                currPot: _potential,
+                              ) !=
+                              null)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FormattedTextWidget(
+                                  text: _reqPotentalRankSelector<
+                                          ModuleTraitDataBundleModel>(
+                                    candidates: part.overrideTraitDataBundle,
+                                    currPot: _potential,
+                                  )!
+                                      .additionalDescription,
+                                  variables: _blackboardListToMap(
+                                      blackboards: _reqPotentalRankSelector<
+                                              ModuleTraitDataBundleModel>(
+                                    candidates: part.overrideTraitDataBundle,
+                                    currPot: _potential,
+                                  )!
+                                          .blackboard),
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                      for (var part
+                          in skadiModule2Data!.phases[_modulePhase].parts)
                         if (part.target == "TALENT")
                           if (_reqPotentalRankSelector<
                                   ModuleTalentDataBundleModel>(
@@ -287,25 +339,26 @@ class _TestPageScreenState extends State<TestPageScreen> {
                                   currPot: _potential,
                                 )!
                                     .name),
-                                Text(_reqPotentalRankSelector<
-                                        ModuleTalentDataBundleModel>(
-                                  candidates:
-                                      part.addOrOverrideTalentDataBundle,
-                                  currPot: _potential,
-                                )!
-                                    .upgradeDescription),
+                                FormattedTextWidget(
+                                  text: _reqPotentalRankSelector<
+                                          ModuleTalentDataBundleModel>(
+                                    candidates:
+                                        part.addOrOverrideTalentDataBundle,
+                                    currPot: _potential,
+                                  )!
+                                      .upgradeDescription,
+                                  variables: _blackboardListToMap(
+                                      blackboards: _reqPotentalRankSelector<
+                                              ModuleTalentDataBundleModel>(
+                                    candidates:
+                                        part.addOrOverrideTalentDataBundle,
+                                    currPot: _potential,
+                                  )!
+                                          .blackboard),
+                                ),
                                 const SizedBox(height: 10),
                               ],
-                            )
-                          else
-                            for (var candidate in part.overrideTraitDataBundle)
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(candidate.additionalDescription),
-                                  const SizedBox(height: 10),
-                                ],
-                              ),
+                            ),
                       for (var attr in skadiModule2Data!
                           .phases[_modulePhase].attributeBlackboard)
                         Text('${attr.key} +${attr.value.toInt()}'),
@@ -314,6 +367,97 @@ class _TestPageScreenState extends State<TestPageScreen> {
                 ),
               )
             : const Text('no data'),
+      ),
+    );
+  }
+}
+
+class FormattedTextWidget extends StatelessWidget {
+  final String text;
+  final Map<String, dynamic> variables;
+
+  const FormattedTextWidget({
+    super.key,
+    required this.text,
+    this.variables = const {},
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<TextSpan> textSpans = [];
+    RegExp separator = RegExp(r"<@.*?>.*?</>");
+
+    List<String> parts = [];
+    int lastIndex = 0;
+    for (Match match in separator.allMatches(text)) {
+      if (lastIndex != match.start) {
+        parts.add(text.substring(lastIndex, match.start));
+      }
+      parts.add(match.group(0)!);
+      lastIndex = match.end;
+    }
+    if (lastIndex != text.length) {
+      parts.add(text.substring(lastIndex));
+    }
+
+    for (String part in parts) {
+      if (part.startsWith('<@')) {
+        String tag = part.substring(0, part.indexOf('>') + 1);
+        String value =
+            part.substring(part.indexOf('>') + 1, part.indexOf('</>'));
+        switch (tag) {
+          case '<@ba.talpu>':
+            textSpans.add(
+              TextSpan(
+                text: value,
+                style: const TextStyle(color: Colors.blue),
+              ),
+            );
+            break;
+          case '<@ba.vup>':
+          case '<@ba.vdown>':
+            String varValue = "";
+            if (value.contains('0%')) {
+              String variable =
+                  value.substring(value.indexOf('{') + 1, value.indexOf(':'));
+              String format =
+                  value.substring(value.indexOf(':') + 1, value.indexOf('}'));
+              if (tag == "<@ba.vup>") {
+                varValue = '+';
+              } else {
+                varValue = '-';
+              }
+              varValue +=
+                  '${(variables[variable] * 100).toStringAsFixed(1)}${format.replaceAll('0', '')}';
+            } else {
+              String variable =
+                  value.substring(value.indexOf('{') + 1, value.indexOf('}'));
+              if (tag == "<@ba.vup>") {
+                varValue = '+';
+              } else {
+                varValue = '-';
+              }
+              varValue += '${variables[variable]}';
+            }
+            textSpans.add(
+              TextSpan(
+                text: varValue,
+                style: TextStyle(
+                  color: tag == "<@ba.vup>" ? Colors.blue : Colors.red,
+                ),
+              ),
+            );
+            break;
+        }
+      } else {
+        textSpans.add(TextSpan(text: part));
+      }
+    }
+
+    return RichText(
+      text: TextSpan(
+        style: DefaultTextStyle.of(context).style,
+        children: textSpans,
       ),
     );
   }
