2af8894bb22e9c63e69e042c523f511fae725d2b
wjlee611 <wjlee611@gmail.com>
2023-02-26 01:14:34 +0900

    fix formatted text widget 1

--- lib/widgets/formatted_text_widget.dart
+++ lib/widgets/formatted_text_widget.dart
@@ -1,4 +1,7 @@
+import 'package:arkhive/constants/sizes.dart';
 import 'package:arkhive/models/common_models.dart';
+import 'package:arkhive/models/font_family.dart';
+import 'package:arkhive/tools/stack.dart';
 import 'package:flutter/material.dart';
 
 class FormattedTextWidget extends StatelessWidget {
@@ -16,8 +19,8 @@ class FormattedTextWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<TextSpan> textSpans = [];
-    RegExp separator = RegExp(r"<.*?>.*?</>");
 
+    RegExp separator = RegExp(r"<.*?>");
     List<String> parts = [];
     int lastIndex = 0;
     for (Match match in separator.allMatches(text)) {
@@ -31,79 +34,173 @@ class FormattedTextWidget extends StatelessWidget {
       parts.add(text.substring(lastIndex));
     }
 
-    for (String part in parts) {
-      if (part.startsWith('<@')) {
-        // variables text
-        String tag = part.substring(0, part.indexOf('>') + 1);
-        String value =
-            part.substring(part.indexOf('>') + 1, part.indexOf('</>'));
-        String varValue = '';
-        String variable = '';
+    const List<String> tags = [
+      '<@ba.kw>',
+      '<@ba.rem>',
+      '<@ba.vdown>',
+      '<@ba.vup>',
+      '<\$ba.buffres>',
+      '<\$ba.camou>',
+      '<\$ba.cold>',
+      '<\$ba.inspire>',
+      '<\$ba.invisible>',
+      '<\$ba.protect>',
+      '<\$ba.root>',
+      '<\$ba.sleep>',
+      '<\$ba.sluggish>',
+      '<\$ba.stun>',
+      '<\$ba.shield>',
+      '<\$ba.binding>',
+      '<\$ba.dt.neural>',
+      '<\$ba.charged>',
+      '<\$ba.strong>',
+      '<\$ba.dt.element>',
+      '<\$ba.fragile>',
+      '<\$ba.frozen>',
+      '<\$ba.overdrive>',
+      '<\$ba.debuff>',
+      '<\$ba.levitate>',
+      '</>',
+    ];
 
-        if (tag == "<@ba.vup>") {
-          if (value != '{duration}') {
-            varValue = '+';
+    DSStack<String> tagsStack = DSStack<String>();
+    List<Widget> widgets = [];
+    for (var part in parts) {
+      for (var word in part.split(' ')) {
+        if (word == '' || word == ' ') continue;
+        var newWord = word;
+        bool isDuration = false;
+        bool isVariable = false;
+        if (word.contains('{') && word.contains('}')) {
+          isVariable = true;
+          var variable = word
+              .substring(word.indexOf('{') + 1, word.indexOf('}'))
+              .split(':')
+              .first;
+          if (variable.contains('duration') || variable.contains('time')) {
+            isDuration = true;
           }
-        } else if (tag == "<@ba.vdown>") {
-          varValue = '-';
+          var value = variables[variable];
+          if (word.contains(':0%')) {
+            newWord =
+                '${(value * 100).toStringAsFixed(1).replaceAll('.0', '')}%';
+          } else {
+            newWord = value.toString().replaceAll('.0', '');
+          }
         }
-
-        if (value.contains(':')) {
-          String format = value
-              .substring(value.indexOf(':') + 1, value.indexOf('}'))
-              .replaceAll('0', '');
-          variable =
-              value.substring(value.indexOf('{') + 1, value.indexOf(':'));
-          varValue +=
-              '${((variables[variable] * 100).toStringAsFixed(1) + format)}';
-        } else if (value.contains('{')) {
-          variable =
-              value.substring(value.indexOf('{') + 1, value.indexOf('}'));
-          varValue += '${variables[variable].toStringAsFixed(1)}';
+        if (tags.contains(newWord)) {
+          // tag
+          if (newWord != '</>') {
+            tagsStack.push(newWord);
+          } else {
+            if (tagsStack.isNotEmpty) {
+              tagsStack.pop();
+            }
+          }
         } else {
-          varValue = value;
-        }
-
-        textSpans.add(
-          TextSpan(
-            text: varValue.replaceAll('.0', ''),
-            style: TextStyle(
-              color: tag == "<@ba.vdown>"
-                  ? Colors.red
-                  : tag == "<@ba.rem>"
-                      ? Colors.yellow.shade800
-                      : Colors.blue,
-            ),
-          ),
-        );
-      } else if (part.startsWith('<\$')) {
-        // link text
-        String tag = part.substring(0, part.indexOf('>') + 1);
-        String value =
-            part.substring(part.indexOf('>') + 1, part.indexOf('</>'));
-        switch (tag) {
-          case '<\$ba.sluggish>':
-          case '<\$ba.debuff>':
-            textSpans.add(
-              TextSpan(
-                text: value,
-                style: const TextStyle(color: Colors.grey),
+          // normal
+          if (tagsStack.isEmpty) {
+            widgets.add(Text(
+              newWord,
+              style: const TextStyle(
+                fontFamily: FontFamily.nanumGothic,
+                fontSize: Sizes.size12,
+                color: Colors.black87,
               ),
-            );
-            break;
+            ));
+          } else {
+            // wrap by tag
+            switch (tagsStack.peek) {
+              case '<@ba.kw>':
+                widgets.add(Text(
+                  newWord,
+                  style: const TextStyle(
+                    fontFamily: FontFamily.nanumGothic,
+                    fontSize: Sizes.size12,
+                    color: Colors.blue,
+                  ),
+                ));
+                break;
+              case '<@ba.rem>':
+                widgets.add(Text(
+                  newWord,
+                  style: TextStyle(
+                    fontFamily: FontFamily.nanumGothic,
+                    fontSize: Sizes.size12,
+                    color: Colors.yellow.shade800,
+                  ),
+                ));
+                break;
+              case '<@ba.vdown>':
+                if (!newWord.contains('+') && !newWord.contains('-')) {
+                  if (!isDuration && isVariable) {
+                    newWord = '-$newWord';
+                  }
+                }
+                widgets.add(Text(
+                  newWord,
+                  style: const TextStyle(
+                    fontFamily: FontFamily.nanumGothic,
+                    fontSize: Sizes.size12,
+                    color: Colors.red,
+                  ),
+                ));
+                break;
+              case '<@ba.vup>':
+                if (!newWord.contains('+') && !newWord.contains('-')) {
+                  if (!isDuration && isVariable) {
+                    newWord = '+$newWord';
+                  }
+                }
+                widgets.add(Text(
+                  newWord,
+                  style: const TextStyle(
+                    fontFamily: FontFamily.nanumGothic,
+                    fontSize: Sizes.size12,
+                    color: Colors.blue,
+                  ),
+                ));
+                break;
+              case '<\$ba.buffres>':
+              case '<\$ba.camou>':
+              case '<\$ba.cold>':
+              case '<\$ba.inspire>':
+              case '<\$ba.invisible>':
+              case '<\$ba.protect>':
+              case '<\$ba.root>':
+              case '<\$ba.sleep>':
+              case '<\$ba.sluggish>':
+              case '<\$ba.stun>':
+              case '<\$ba.shield>':
+              case '<\$ba.binding>':
+              case '<\$ba.dt.neural>':
+              case '<\$ba.charged>':
+              case '<\$ba.strong>':
+              case '<\$ba.dt.element>':
+              case '<\$ba.fragile>':
+              case '<\$ba.frozen>':
+              case '<\$ba.overdrive>':
+              case '<\$ba.debuff>':
+              case '<\$ba.levitate>':
+                // 추후 기능 추가
+                widgets.add(Text(
+                  newWord,
+                  style: const TextStyle(
+                    fontFamily: FontFamily.nanumGothic,
+                    fontSize: Sizes.size12,
+                    color: Colors.grey,
+                  ),
+                ));
+                break;
+            }
+          }
         }
-      } else {
-        // default text
-        textSpans.add(TextSpan(text: part));
       }
     }
 
-    return RichText(
-      text: TextSpan(
-        style: DefaultTextStyle.of(context).style,
-        children: textSpans,
-      ),
-      textAlign: center ? TextAlign.center : TextAlign.start,
+    return Wrap(
+      spacing: Sizes.size3,
+      children: widgets,
     );
   }
 }
