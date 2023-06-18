23546dd3baad3326cf317e2428753a7056d637c9
wjlee611 <wjlee611@gmail.com>
2023-06-18 18:59:26 +0900

    add item_screen

---
+++ lib/screens/item/item_screen.dart
@@ -0,0 +1,63 @@
+import 'package:arkhive/models/item_model.dart';
+import 'package:flutter/material.dart';
+
+class ItemScreen extends StatefulWidget {
+  const ItemScreen({super.key});
+
+  @override
+  State<ItemScreen> createState() => _ItemScreenState();
+}
+
+class _ItemScreenState extends State<ItemScreen> {
+  final _searchController = TextEditingController();
+  late Future<List<ItemModel>> _futureItems;
+  bool _isLoading = false;
+  final String _searchKeyword = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _futureItems = futureItems();
+  }
+
+  Future<List<ItemModel>> futureItems() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    List<ItemModel> result = [];
+
+    return result;
+  }
+
+  List<ItemModel> _runFilter(List<ItemModel> list) {
+    return list.where((item) {
+      final nameLower = item.name.toLowerCase();
+      final searchLower = _searchKeyword.toLowerCase();
+
+      return nameLower.contains(searchLower);
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(),
+          SliverFillRemaining(
+            child: FutureBuilder(
+              future: _futureItems,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var filteredLists = _runFilter(snapshot.data!);
+                  return const CustomScrollView();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
