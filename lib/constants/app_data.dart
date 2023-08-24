040f7217ed21d0e3c9e2b94edb19a74e3ecf76e0
wjlee611 <wjlee611@gmail.com>
2023-08-24 15:50:52 +0900

    update gameData

--- lib/constants/app_data.dart
+++ lib/constants/app_data.dart
@@ -1,7 +1,7 @@
 class AppData {
-  static const version = '1.0.0';
+  static const version = '1.0.230824';
   static const gameVersion = '17.9.62';
-  static const penguinDate = '2023.08.23';
+  static const penguinDate = '2023.08.24';
 
   static const nullStr = 'N/A';
 }
