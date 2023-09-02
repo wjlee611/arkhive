660624cdf0a7c1dfbd940a90d753f49130b34a39
wjlee611 <wjlee611@gmail.com>
2023-09-02 22:07:48 +0900

    design setting screen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:arkhive/constants/gaps.dart';
 import 'package:arkhive/constants/sizes.dart';
 import 'package:arkhive/screens/settings/widgets/setting_db_region_widget.dart';
 import 'package:arkhive/widgets/app_font.dart';
@@ -19,16 +20,38 @@ class SettingsScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.blueGrey.shade700,
       ),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                AppFont('데이터베이스 지역'),
-                SettingDBRegionWidget(),
-              ],
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const AppFont(
+                    '데이터베이스 지역',
+                    fontSize: Sizes.size14,
+                  ),
+                  Gaps.h10,
+                  Flexible(
+                    child: Container(
+                      height: Sizes.size1,
+                      color: Theme.of(context).shadowColor,
+                    ),
+                  ),
+                  Gaps.h10,
+                  const SettingDBRegionWidget(),
+                ],
+              ),
+              Gaps.v3,
+              AppFont(
+                '* 위 설정 변경시 메인 화면으로 돌아가게 됩니다.',
+                color: Theme.of(context).textTheme.labelSmall!.color,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/settings/widgets/setting_db_region_widget.dart
+++ lib/screens/settings/widgets/setting_db_region_widget.dart
@@ -23,44 +23,58 @@ class SettingDBRegionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingCubit, SettingState>(
-      builder: (context, state) => PopupMenuButton(
-        initialValue: state.settings.dbRegion,
-        onSelected: (dbRegion) => _onSelected(context, dbRegion),
-        offset: const Offset(0, 0),
-        icon: BlocBuilder<SettingCubit, SettingState>(
-          buildWhen: (previous, current) =>
-              previous.settings.dbRegion != current.settings.dbRegion,
-          builder: (context, state) => Container(
-            child: AppFont(
+      builder: (context, state) => Container(
+        width: Sizes.size96,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: Sizes.size1,
+            color: Colors.yellow.shade800,
+          ),
+          borderRadius: BorderRadius.circular(Sizes.size10),
+        ),
+        child: PopupMenuButton(
+          initialValue: state.settings.dbRegion,
+          onSelected: (dbRegion) => _onSelected(context, dbRegion),
+          offset: const Offset(Sizes.size1, 0),
+          padding: EdgeInsets.zero,
+          splashRadius: 0.001,
+          icon: BlocBuilder<SettingCubit, SettingState>(
+            buildWhen: (previous, current) =>
+                previous.settings.dbRegion != current.settings.dbRegion,
+            builder: (context, state) => AppFont(
               state.settings.dbRegion?.path ?? Region.kr.path,
-            ),
-          ),
-        ),
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            width: Sizes.size5,
-            color: Colors.blueGrey.shade700,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(Sizes.size10)),
-        ),
-        color: Theme.of(context).primaryColor,
-        elevation: 0,
-        itemBuilder: (context) => [
-          PopupMenuItem(
-            value: Region.kr,
-            child: AppFont(
-              Region.kr.path,
+              fontWeight: FontWeight.w700,
               fontSize: Sizes.size14,
             ),
           ),
-          PopupMenuItem(
-            value: Region.cn,
-            child: AppFont(
-              Region.cn.path,
-              fontSize: Sizes.size14,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              width: Sizes.size5,
+              color: Colors.blueGrey.shade700,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(Sizes.size10),
             ),
           ),
-        ],
+          color: Theme.of(context).primaryColor,
+          elevation: 0,
+          itemBuilder: (context) => [
+            PopupMenuItem(
+              value: Region.kr,
+              child: AppFont(
+                Region.kr.path,
+                fontSize: Sizes.size14,
+              ),
+            ),
+            PopupMenuItem(
+              value: Region.cn,
+              child: AppFont(
+                Region.cn.path,
+                fontSize: Sizes.size14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
