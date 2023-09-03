096792e2f31fb61e1ec581044d15aaeb832352f1
wjlee611 <wjlee611@gmail.com>
2023-09-03 21:21:52 +0900

    remodeling operator 2 - module

--- lib/models/operator/module_model.dart
+++ lib/models/operator/module_model.dart
@@ -1,137 +1,256 @@
+import 'package:arkhive/models/abstract_class/abs_candidates.dart';
+import 'package:arkhive/models/common/item_cost_model.dart';
 import 'package:arkhive/models/common/talent_blackboard_model.dart';
-import 'package:arkhive/models/common_models.dart';
-import 'package:arkhive/models/operator/operator_model.dart';
+import 'package:arkhive/models/common/unlock_condition_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-class ModuleModel {
-  final String? uniEquipId,
-      uniEquipName,
-      uniEquipIcon,
-      typeIcon,
-      charId,
-      equipShiningColor;
-  final List<String> missionList;
-  final Map<String, List<EvolveCostModel>> itemCost;
+part 'module_model.g.dart';
 
-  ModuleModel.fromJson(Map<String, dynamic> json)
-      : uniEquipId = json['uniEquipId'],
-        uniEquipName = json['uniEquipName'],
-        uniEquipIcon = json['uniEquipIcon'],
-        typeIcon = json['typeIcon'],
-        charId = json['charId'],
-        equipShiningColor = json['equipShiningColor'],
-        missionList = [
-          if (json['missionList'] != null)
-            for (var data in json['missionList']) data
-        ],
-        itemCost = {
-          if (json['itemCost'] != null)
-            for (var key in json['itemCost'].keys)
-              key: [
-                for (var data in json['itemCost'][key])
-                  EvolveCostModel.fromJson(data)
-              ]
-        };
+@JsonSerializable(createToJson: false)
+class ModuleModel extends Equatable {
+  final String? uniEquipId;
+  final String? uniEquipName;
+  final String? uniEquipIcon;
+  final String? uniEquipDesc;
+  final String? typeIcon;
+  final String? typeName1;
+  final String? typeName2;
+  final String? equipShiningColor;
+  final int? showEvolvePhase;
+  final int? unlockEvolvePhase;
+  final String? charId;
+  final String? tmplId;
+  final int? showLevel;
+  final int? unlockLevel;
+  final int? unlockFavorPoint;
+  final List<String>? missionList;
+  final Map<String, List<ItemCostModel>>? itemCost;
+  final String? type;
+  final int? uniEquipGetTime;
+  final int? charEquipOrder;
+
+  const ModuleModel({
+    this.uniEquipId,
+    this.uniEquipName,
+    this.uniEquipIcon,
+    this.uniEquipDesc,
+    this.typeIcon,
+    this.typeName1,
+    this.typeName2,
+    this.equipShiningColor,
+    this.showEvolvePhase,
+    this.unlockEvolvePhase,
+    this.charId,
+    this.tmplId,
+    this.showLevel,
+    this.unlockLevel,
+    this.unlockFavorPoint,
+    this.missionList,
+    this.itemCost,
+    this.type,
+    this.uniEquipGetTime,
+    this.charEquipOrder,
+  });
+
+  factory ModuleModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleModelFromJson(json);
+
+  @override
+  List<Object?> get props => [
+        uniEquipId,
+        uniEquipName,
+        uniEquipIcon,
+        uniEquipDesc,
+        typeIcon,
+        typeName1,
+        typeName2,
+        equipShiningColor,
+        showEvolvePhase,
+        unlockEvolvePhase,
+        charId,
+        tmplId,
+        showLevel,
+        unlockLevel,
+        unlockFavorPoint,
+        missionList,
+        itemCost,
+        type,
+        uniEquipGetTime,
+        charEquipOrder,
+      ];
 }
 
-class ModuleMissionModel {
-  final String? desc, jumpStageId;
+/// Module Data below ///
+@JsonSerializable(createToJson: false)
+class ModuleDataModel extends Equatable {
+  final List<ModuleDataPhasesModel>? phases;
 
-  ModuleMissionModel.fromJson(Map<String, dynamic> json)
-      : desc = json['desc'],
-        jumpStageId = json['jumpStageId'];
+  const ModuleDataModel({this.phases});
+
+  factory ModuleDataModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataModelFromJson(json);
+
+  @override
+  List<Object?> get props => [phases];
 }
 
-// MODULE SPEC DATA
-class ModuleDataModel {
-  final List<ModuleDataPhaseModel> phases;
-
-  ModuleDataModel.fromJson(Map<String, dynamic> json)
-      : phases = [
-          if (json['phases'] != null)
-            for (var data in json['phases']) ModuleDataPhaseModel.fromJson(data)
-        ];
-}
-
-class ModuleDataPhaseModel {
+@JsonSerializable(createToJson: false)
+class ModuleDataPhasesModel extends Equatable {
   final int? equipLevel;
-  final List<ModuleDataPartsModel> parts;
-  final List<TalentBlackboardModel> attributeBlackboard;
+  final List<ModuleDataPartsModel>? parts;
+  final List<TalentBlackboardModel>? attributeBlackboard;
+  // "tokenAttributeBlackboard": {}
 
-  ModuleDataPhaseModel.fromJson(Map<String, dynamic> json)
-      : equipLevel = json['equipLevel'],
-        parts = [
-          if (json['parts'] != null)
-            for (var data in json['parts']) ModuleDataPartsModel.fromJson(data)
-        ],
-        attributeBlackboard = [
-          if (json['attributeBlackboard'] != null)
-            for (var data in json['attributeBlackboard'])
-              TalentBlackboardModel.fromJson(data)
-        ];
+  const ModuleDataPhasesModel({
+    this.equipLevel,
+    this.parts,
+    this.attributeBlackboard,
+  });
+
+  factory ModuleDataPhasesModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataPhasesModelFromJson(json);
+
+  @override
+  List<Object?> get props => [
+        equipLevel,
+        parts,
+        attributeBlackboard,
+      ];
 }
 
-class ModuleDataPartsModel {
+@JsonSerializable(createToJson: false)
+class ModuleDataPartsModel extends Equatable {
+  final String? resKey;
   final String? target;
   final bool? isToken;
-  final List<ModuleTalentDataBundleModel> addOrOverrideTalentDataBundle;
-  final List<ModuleTraitDataBundleModel> overrideTraitDataBundle;
+  final ModuleDataTalentBundleModel? addOrOverrideTalentDataBundle;
+  final ModuleDataTraitBundleModel? overrideTraitDataBundle;
 
-  ModuleDataPartsModel.fromJson(Map<String, dynamic> json)
-      : target = json['target'],
-        isToken = json['isToken'],
-        addOrOverrideTalentDataBundle = [
-          if (json['addOrOverrideTalentDataBundle']['candidates'] != null)
-            for (var data in json['addOrOverrideTalentDataBundle']
-                ['candidates'])
-              ModuleTalentDataBundleModel.fromJson(data)
-        ],
-        overrideTraitDataBundle = [
-          if (json['overrideTraitDataBundle']['candidates'] != null)
-            for (var data in json['overrideTraitDataBundle']['candidates'])
-              ModuleTraitDataBundleModel.fromJson(data)
-        ];
+  const ModuleDataPartsModel({
+    this.resKey,
+    this.target,
+    this.isToken,
+    this.addOrOverrideTalentDataBundle,
+    this.overrideTraitDataBundle,
+  });
+
+  factory ModuleDataPartsModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataPartsModelFromJson(json);
+
+  @override
+  List<Object?> get props => [
+        resKey,
+        target,
+        isToken,
+        addOrOverrideTalentDataBundle,
+        overrideTraitDataBundle,
+      ];
 }
 
-class ModuleTalentDataBundleModel implements PotentialRank {
-  final String? upgradeDescription, name;
+@JsonSerializable(createToJson: false)
+class ModuleDataTalentBundleModel extends Equatable {
+  final List<ModuleDataTalentCandidateModel>? candidates;
+
+  const ModuleDataTalentBundleModel({this.candidates});
+
+  factory ModuleDataTalentBundleModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataTalentBundleModelFromJson(json);
+
+  @override
+  List<Object?> get props => [candidates];
+}
+
+@JsonSerializable(createToJson: false)
+class ModuleDataTalentCandidateModel extends ABSCandidate {
+  final bool? displayRangeId;
+  final String? upgradeDescription;
   final int? talentIndex;
-  @override
-  final int? requiredPotentialRank;
-  @override
-  final OperatorUnlockCondModel unlockCondition;
-  final List<TalentBlackboardModel> blackboard;
+  final String? prefabKey;
+  final String? name;
+  final String? description;
+  final String? rangeId;
+  final List<TalentBlackboardModel>? blackboard;
 
-  ModuleTalentDataBundleModel.fromJson(Map<String, dynamic> json)
-      : upgradeDescription = json['upgradeDescription'],
-        talentIndex = json['talentIndex'],
-        unlockCondition =
-            OperatorUnlockCondModel.fromJson(json['unlockCondition'] ?? {}),
-        requiredPotentialRank = json['requiredPotentialRank'],
-        name = json['name'],
-        blackboard = [
-          if (json['blackboard'] != null)
-            for (var data in json['blackboard'])
-              TalentBlackboardModel.fromJson(data)
-        ];
+  const ModuleDataTalentCandidateModel({
+    required UnlockConditionModel unlockCondition,
+    required int requiredPotentialRank,
+    this.displayRangeId,
+    this.upgradeDescription,
+    this.talentIndex,
+    this.prefabKey,
+    this.name,
+    this.description,
+    this.rangeId,
+    this.blackboard,
+  }) : super(
+          unlockCondition: unlockCondition,
+          requiredPotentialRank: requiredPotentialRank,
+        );
+
+  factory ModuleDataTalentCandidateModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataTalentCandidateModelFromJson(json);
+
+  @override
+  List<Object?> get props => [
+        unlockCondition,
+        requiredPotentialRank,
+        displayRangeId,
+        upgradeDescription,
+        talentIndex,
+        prefabKey,
+        name,
+        description,
+        rangeId,
+        blackboard,
+      ];
 }
 
-class ModuleTraitDataBundleModel implements PotentialRank {
-  final String? additionalDescription, overrideDescripton;
-  @override
-  final OperatorUnlockCondModel unlockCondition;
-  @override
-  final int? requiredPotentialRank;
-  final List<TalentBlackboardModel> blackboard;
+@JsonSerializable(createToJson: false)
+class ModuleDataTraitBundleModel extends Equatable {
+  final List<ModuleDataTraitCandidateModel>? candidates;
 
-  ModuleTraitDataBundleModel.fromJson(Map<String, dynamic> json)
-      : additionalDescription = json['additionalDescription'],
-        overrideDescripton = json['overrideDescripton'],
-        unlockCondition =
-            OperatorUnlockCondModel.fromJson(json['unlockCondition'] ?? {}),
-        requiredPotentialRank = json['requiredPotentialRank'],
-        blackboard = [
-          if (json['blackboard'] != null)
-            for (var data in json['blackboard'])
-              TalentBlackboardModel.fromJson(data)
-        ];
+  const ModuleDataTraitBundleModel({this.candidates});
+
+  factory ModuleDataTraitBundleModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataTraitBundleModelFromJson(json);
+
+  @override
+  List<Object?> get props => [candidates];
+}
+
+@JsonSerializable(createToJson: false)
+class ModuleDataTraitCandidateModel extends ABSCandidate {
+  final String? additionalDescription;
+  final List<TalentBlackboardModel>? blackboard;
+  final String? overrideDescripton;
+  final String? prefabKey;
+  final String? rangeId;
+
+  const ModuleDataTraitCandidateModel({
+    required UnlockConditionModel unlockCondition,
+    required int requiredPotentialRank,
+    this.additionalDescription,
+    this.blackboard,
+    this.overrideDescripton,
+    this.prefabKey,
+    this.rangeId,
+  }) : super(
+          unlockCondition: unlockCondition,
+          requiredPotentialRank: requiredPotentialRank,
+        );
+
+  factory ModuleDataTraitCandidateModel.fromJson(Map<String, dynamic> json) =>
+      _$ModuleDataTraitCandidateModelFromJson(json);
+
+  @override
+  List<Object?> get props => [
+        unlockCondition,
+        requiredPotentialRank,
+        additionalDescription,
+        blackboard,
+        overrideDescripton,
+        prefabKey,
+        rangeId,
+      ];
 }
---
+++ lib/models/operator/module_model.g.dart
@@ -0,0 +1,129 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'module_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ModuleModel _$ModuleModelFromJson(Map<String, dynamic> json) => ModuleModel(
+      uniEquipId: json['uniEquipId'] as String?,
+      uniEquipName: json['uniEquipName'] as String?,
+      uniEquipIcon: json['uniEquipIcon'] as String?,
+      uniEquipDesc: json['uniEquipDesc'] as String?,
+      typeIcon: json['typeIcon'] as String?,
+      typeName1: json['typeName1'] as String?,
+      typeName2: json['typeName2'] as String?,
+      equipShiningColor: json['equipShiningColor'] as String?,
+      showEvolvePhase: json['showEvolvePhase'] as int?,
+      unlockEvolvePhase: json['unlockEvolvePhase'] as int?,
+      charId: json['charId'] as String?,
+      tmplId: json['tmplId'] as String?,
+      showLevel: json['showLevel'] as int?,
+      unlockLevel: json['unlockLevel'] as int?,
+      unlockFavorPoint: json['unlockFavorPoint'] as int?,
+      missionList: (json['missionList'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      itemCost: (json['itemCost'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(
+            k,
+            (e as List<dynamic>)
+                .map((e) => ItemCostModel.fromJson(e as Map<String, dynamic>))
+                .toList()),
+      ),
+      type: json['type'] as String?,
+      uniEquipGetTime: json['uniEquipGetTime'] as int?,
+      charEquipOrder: json['charEquipOrder'] as int?,
+    );
+
+ModuleDataModel _$ModuleDataModelFromJson(Map<String, dynamic> json) =>
+    ModuleDataModel(
+      phases: (json['phases'] as List<dynamic>?)
+          ?.map(
+              (e) => ModuleDataPhasesModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+ModuleDataPhasesModel _$ModuleDataPhasesModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataPhasesModel(
+      equipLevel: json['equipLevel'] as int?,
+      parts: (json['parts'] as List<dynamic>?)
+          ?.map((e) => ModuleDataPartsModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      attributeBlackboard: (json['attributeBlackboard'] as List<dynamic>?)
+          ?.map(
+              (e) => TalentBlackboardModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+ModuleDataPartsModel _$ModuleDataPartsModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataPartsModel(
+      resKey: json['resKey'] as String?,
+      target: json['target'] as String?,
+      isToken: json['isToken'] as bool?,
+      addOrOverrideTalentDataBundle: json['addOrOverrideTalentDataBundle'] ==
+              null
+          ? null
+          : ModuleDataTalentBundleModel.fromJson(
+              json['addOrOverrideTalentDataBundle'] as Map<String, dynamic>),
+      overrideTraitDataBundle: json['overrideTraitDataBundle'] == null
+          ? null
+          : ModuleDataTraitBundleModel.fromJson(
+              json['overrideTraitDataBundle'] as Map<String, dynamic>),
+    );
+
+ModuleDataTalentBundleModel _$ModuleDataTalentBundleModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataTalentBundleModel(
+      candidates: (json['candidates'] as List<dynamic>?)
+          ?.map((e) => ModuleDataTalentCandidateModel.fromJson(
+              e as Map<String, dynamic>))
+          .toList(),
+    );
+
+ModuleDataTalentCandidateModel _$ModuleDataTalentCandidateModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataTalentCandidateModel(
+      unlockCondition: UnlockConditionModel.fromJson(
+          json['unlockCondition'] as Map<String, dynamic>),
+      requiredPotentialRank: json['requiredPotentialRank'] as int,
+      displayRangeId: json['displayRangeId'] as bool?,
+      upgradeDescription: json['upgradeDescription'] as String?,
+      talentIndex: json['talentIndex'] as int?,
+      prefabKey: json['prefabKey'] as String?,
+      name: json['name'] as String?,
+      description: json['description'] as String?,
+      rangeId: json['rangeId'] as String?,
+      blackboard: (json['blackboard'] as List<dynamic>?)
+          ?.map(
+              (e) => TalentBlackboardModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+ModuleDataTraitBundleModel _$ModuleDataTraitBundleModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataTraitBundleModel(
+      candidates: (json['candidates'] as List<dynamic>?)
+          ?.map((e) =>
+              ModuleDataTraitCandidateModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+ModuleDataTraitCandidateModel _$ModuleDataTraitCandidateModelFromJson(
+        Map<String, dynamic> json) =>
+    ModuleDataTraitCandidateModel(
+      unlockCondition: UnlockConditionModel.fromJson(
+          json['unlockCondition'] as Map<String, dynamic>),
+      requiredPotentialRank: json['requiredPotentialRank'] as int,
+      additionalDescription: json['additionalDescription'] as String?,
+      blackboard: (json['blackboard'] as List<dynamic>?)
+          ?.map(
+              (e) => TalentBlackboardModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      overrideDescripton: json['overrideDescripton'] as String?,
+      prefabKey: json['prefabKey'] as String?,
+      rangeId: json['rangeId'] as String?,
+    );
