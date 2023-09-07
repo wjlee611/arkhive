5338a57884991a8e53f1a0b5c8f2452dd51d8163
wjlee611 <wjlee611@gmail.com>
2023-09-07 11:54:09 +0900

    v1.1.1 - fix bug: internet permission (android)

--- lib/constants/app_data.dart
+++ lib/constants/app_data.dart
@@ -1,5 +1,5 @@
 class AppData {
-  static const version = '1.1.0';
+  static const version = '1.1.1';
   static const gameVersion = '18.9.81';
   static const cnGameVersion = '2.0.81';
   static const penguinDate = '2023.09.07';
