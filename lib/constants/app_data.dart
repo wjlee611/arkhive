5b72e2f0d813e42ad11f14f76a27d4b4cff6d6a7
wjlee611 <wjlee611@gmail.com>
2023-11-07 21:42:00 +0900

    kr data update

--- lib/constants/app_data.dart
+++ lib/constants/app_data.dart
@@ -2,7 +2,7 @@ class AppData {
   static const version = '1.2.0';
   static const gameVersion = '19.0.01';
   static const cnGameVersion = '2.0.81';
-  static const penguinDate = '2023.10.29';
+  static const penguinDate = '2023.11.07';
 
   static const nullStr = 'N/A';
 }
